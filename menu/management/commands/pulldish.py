c3ed05c126a977988a745b1b2e528600e5720f41
blankxx <Shinima16>
2022-07-11 14:46:51 +0200

    creating html page for menus

--- menu/management/commands/pulldish.py
+++ menu/management/commands/pulldish.py
@@ -9,16 +9,16 @@ def clear_db_dish():
 
 class Command(BaseCommand):
 
-    def handle(self):
+    def handle(self, *args, **kwargs):
         clear_db_dish()
         try:
             with open('menu/dish.json', 'r') as dish_file:
                 dish_data = json.load(dish_file)
-                for dish_name in dish_data['name']:
+                for dish_name, recipe in dish_data['name'] and dish_data['recipe']:
                     if "'" in dish_name:
-                        Dish(name=dish_name.replace("'", " ")).save()
+                        Dish(dish_name=dish_name.replace("'", " "), recipe=recipe).save()
                     else:
-                        Dish(name=dish_name).save()
+                        Dish(dish_name=dish_name, recipe=recipe).save()
                     self.stdout.write(f"{self.style.SUCCESS(dish_name)} ajouté à la base de données")
                 self.stdout.write(self.style.SUCCESS('Les plats ont correctement été ajoutés dans la base de données'))
         except ValueError:
--- menu/urls.py
+++ menu/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 app_name = "menu"
 urlpatterns = [
-    path("", views.home, name="home")
+    path("", views.home, name="home"),
+    path("menu_am", views.menu_am, name="menu-am"),
+    path("menu_pm", views.menu_pm, name="menu-pm")
 ]
-
