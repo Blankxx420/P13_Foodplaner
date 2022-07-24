aa71e8445ad57dc243d791355b3571f85aa2eb88
blankxx <Shinima16>
2022-07-24 09:38:51 +0200

    correction of bug that can retrieve name views

--- menu/urls.py
+++ menu/urls.py
@@ -4,6 +4,6 @@ from . import views
 app_name = "menu"
 urlpatterns = [
     path("", views.home, name="home"),
-    path("menu_am", views.menu_am, name="menu-am"),
-    path("menu_pm", views.menu_pm, name="menu-pm")
+    path("menu_am", views.menu_am, name="menu_am"),
+    path("menu_pm", views.menu_pm, name="menu_pm")
 ]
