48957e8c45516068c1fcb128f186045dc621d667
blankxx <Shinima16>
2022-07-27 11:09:52 +0200

    adding requirements.txt for travis

--- Foodplaner/settings/travis.py
+++ Foodplaner/settings/travis.py
@@ -4,7 +4,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY", SECRET_KEY)
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': '',
         'USER': 'postgres',
         'PASSWORD': '',
