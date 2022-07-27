90fc87d85e7ea33f44b817bfa70521719e8cee88
blankxx <Shinima16>
2022-07-27 10:38:41 +0200

    implementing travis

---
+++ Foodplaner/settings/travis.py
@@ -0,0 +1,14 @@
+from settings import *
+
+SECRET_KEY = os.environ.get("SECRET_KEY", SECRET_KEY)
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': '',
+        'USER': 'postgres',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+    },
+}
