8ce71a15742e57eacc60d60269098f50d07ca353
blankxx <Shinima16>
2022-07-29 09:34:00 +0200

    adding model on the admin panel

--- menu/admin.py
+++ menu/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-from menu.models import Dish
+from menu.models import Dish, Date, Menu
 admin.site.register(Dish)
+admin.site.register(Date)
+admin.site.register(Menu)
